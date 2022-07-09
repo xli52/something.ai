407f90ad0294ab974a9cc0af4797557d51f1f64f
Joshua Cheung <contact.cjoshua@gmail.com>
2022-07-09 14:26:04 -0700

    remove cartoongirl glb

--- client/src/components/Home/ModelDisplay.jsx
+++ client/src/components/Home/ModelDisplay.jsx
@@ -2,7 +2,6 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Avatar from "../../avatars/Avatar";
-import CartoonGirl from "../../avatars/CartoonGirl";
 
 export default function ModelDisplay(props) {
   return (
@@ -18,18 +17,5 @@ export default function ModelDisplay(props) {
       </Suspense>
       <OrbitControls />
     </Canvas>
-
-    // <Canvas
-    //   className="chat-scene-canvas"
-    //   camera={{ fov: 20, near: 10, far: 2000, position: [0, 0, 600] }}
-    //   style={{ width: "70%", margin: "auto" }}
-    // >
-    //   <ambientLight intensity={1.25} />
-    //   <directionalLight intensity={0.4} />
-    //   <Suspense fallback={null}>
-    //     <CartoonGirl />
-    //   </Suspense>
-    //   <OrbitControls />
-    // </Canvas>
   );
 }
