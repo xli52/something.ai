b1c54c3e294a8e35337dab41a3acc4a798ad7f98
Joshua Cheung <contact.cjoshua@gmail.com>
2022-07-09 14:20:42 -0700

    Add avatar in Home page

--- client/src/components/Home/ModelDisplay.jsx
+++ client/src/components/Home/ModelDisplay.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Avatar from "../../avatars/Avatar";
+import CartoonGirl from "../../avatars/CartoonGirl";
 
-export default function ModelDisplay() {
-  return <div>ModelDisplay</div>;
+export default function ModelDisplay(props) {
+  return (
+    <Canvas
+      className="chat-scene-canvas"
+      camera={{ fov: 20, near: 0.02, far: 1000, position: [0.1, 0.1, 3.5] }}
+      style={{ width: "70%", margin: "auto" }}
+    >
+      <ambientLight intensity={1.25} />
+      <directionalLight intensity={0.4} />
+      <Suspense fallback={null}>
+        <Avatar />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+
+    // <Canvas
+    //   className="chat-scene-canvas"
+    //   camera={{ fov: 20, near: 10, far: 2000, position: [0, 0, 600] }}
+    //   style={{ width: "70%", margin: "auto" }}
+    // >
+    //   <ambientLight intensity={1.25} />
+    //   <directionalLight intensity={0.4} />
+    //   <Suspense fallback={null}>
+    //     <CartoonGirl />
+    //   </Suspense>
+    //   <OrbitControls />
+    // </Canvas>
+  );
 }
--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Intro from "./Intro";
 import ModelDisplay from "./ModelDisplay";
 
-import "./syles.scss";
+import "./styles.scss";
 
 export default function Home() {
   return (
