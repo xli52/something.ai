55418453ad4d27d2857a1ffa5de1b409f73ab0d5
Joshua Cheung <contact.cjoshua@gmail.com>
2022-07-09 12:02:34 -0700

    Home index Intro layout complete

--- client/src/components/home/Intro.jsx
+++ client/src/components/home/Intro.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Intro() {
   return (
     <div className="introBody">
-      <h1>What to chat ?</h1>
-      <div className="line"></div>
-      <h2>But all your friend busy ?</h2>
-      <div className="line"></div>
-      <h2>You have a secert cannot tell anyone ?</h2>
-      <h2>or, you get no friend</h2>
-      <h2>No worry, We got it !</h2>
+      <h1 className="center">What to chat ?</h1>
+      <div className="line right"></div>
+      <h2 className="right">But all your friend busy ?</h2>
+      <div className="line left"></div>
+      <h2 className="left">You have a secert cannot tell anyone ?</h2>
+      <h2 className="right">or, you get no friend ....</h2>
+      <h2 className="center">No worry, We get it !</h2>
       <button className="btn">Let's have a talk</button>
     </div>
   );
