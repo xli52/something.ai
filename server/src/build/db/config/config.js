09a60a9124b5393e3bcfe5768a82d29eba2ce960
Alex Cheung <alexc.sfu@gmail.com>
2022-07-10 01:10:39 -0700

    installed sequelize-cli for migrations and model setup for ORM

---
+++ server/src/build/db/config/config.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+exports.default = {
+    development: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        dialect: "postgres",
+    },
+};
---
+++ server/src/db/config/config.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: "postgres",
+  },
+};
