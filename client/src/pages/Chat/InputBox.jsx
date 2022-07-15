760d6ffac3015b3a219b7f03e5e19a9cbd89d7e6
Joshua Cheung <contact.cjoshua@gmail.com>
2022-07-15 10:04:33 -0700

    commit for new pull

--- client/src/pages/Chat/InputBox.jsx
+++ client/src/pages/Chat/InputBox.jsx
@@ -1,28 +1,43 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-export default function InputBox({ setText, textMode }) {
-
-  const [message, setMessage] = useState('');
+export default function InputBox({ setText, textMode, setBotText }) {
+  const [input, setInput] = useState("");
 
   function send(event) {
-    if (message) {
-      event.preventDefault();
-      setText(message);
-      setMessage('');
-      // Send message to api here
-      
+    event.preventDefault();
+    setText(input);
+    setInput("");
+    setBotText("em . . .");
+    if (input) {
+      console.log(input);
+      return axios({
+        method: "POST",
+        url: "/api/openai/textToSpeech",
+        data: JSON.stringify({ input }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          console.log("React render stage", res);
+          // setAudio(`/${res.data.audioID}.mp3`);
+
+          setBotText(res.data.aiText);
+        })
+        .catch((e) => console.log(e.input));
     }
-  };
+  }
 
   return (
     <div className="input-box-container__text">
       <div className="chat-bar">
-        <form className="chat-bar__message" onSubmit={event => send(event)}>
+        <form className="chat-bar__message" onSubmit={(event) => send(event)}>
           <input
             className="chat-bar__input"
             type="text"
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
             placeholder="Chat Message..."
           />
         </form>
--- client/src/pages/Chat/index.jsx
+++ client/src/pages/Chat/index.jsx
@@ -6,32 +6,21 @@ import Bubble from "../../components/Bubble";
 import "./styles.scss";
 
 export default function Chat(props) {
-
-  const [userText, setUserText] = useState('');
-  const [botText, setBotText] = useState('Hello Human!');
+  const [userText, setUserText] = useState("");
+  const [botText, setBotText] = useState("Hello Human!");
 
   return (
     <main>
       <div className="chat-container">
         <div className="chat-scene-container">
           <ChatScene />
-          <InputBox setText={setUserText} />
-          {userText &&
-            <Bubble
-              text={userText}
-              user
-              bot={false}
-              setText={setUserText}
-            />
-          }
-          {botText &&
-            <Bubble
-              text={botText}
-              user={false}
-              bot
-              setText={setBotText}
-            />
-          }
+          <InputBox setText={setUserText} setBotText={setBotText} />
+          {userText && (
+            <Bubble text={userText} user bot={false} setText={setUserText} />
+          )}
+          {botText && (
+            <Bubble text={botText} user={false} bot setText={setBotText} />
+          )}
         </div>
       </div>
     </main>
